91c1bf8c73d79e4d30f4ec3f531918fe523c25b6
Ascentum <84969382+Ascentum@users.noreply.github.com>
2021-06-22 18:36:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-exports.Token = "ODUxNzA4Nzc3NTEzNzQ2NDUy.YL8NnA.Yx8C0Rm5jUa7CboiW6MopP01j4g"; //Discord Bot Token
+exports.Token = "Bot Token"; //Discord Bot Token
 
-exports.Color = "#2F3136"; //Default Embed Color
+exports.Color = "RANDOM"; //Default Embed Color Must Be Written in Caps
 
-exports.Channel = "856764116718321674"; //Channel Id Of Suggestion Channel!
+exports.Channel = "Channel ID"; //Channel Id Of Suggestion Channel!
 
 exports.Success = "<:success:852406442387963965>"; //Emoji Id Success
 
